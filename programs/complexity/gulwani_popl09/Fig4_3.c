419635f3a85f631c69ab684761c7db4b72e59882
timosantonopoulos <devnull@localhost>
2017-02-21 17:05:53 -0500

    getting correct bounds with assert statements

--- programs/complexity/gulwani_popl09/Fig4_3.c
+++ programs/complexity/gulwani_popl09/Fig4_3.c
@@ -24,6 +24,8 @@ int mainQ(int n, int m){
      //dig2:  y <= 0, -x <= 0, n - t <= 0, -t + x <= 0, n*t - t*x == 0, -y <= 0
      //NOTE: *** does this eq  n*t - t*x == 0  give us anything useful about the complexity?  ***
      //Timos: I tried this example with DIG1 and I got m*n + n - t == 0 which looks exactly right. Is there something missing above?
+     //Timos: UPDATE: With the assert statements above, the correct bound is given, although if we leave the variable x in the traces, we get this:
+     //m*x - t + x == 0, n - x == 0, y == 0, which implies m*n - t + n == 0
      return 0;
 }
 
