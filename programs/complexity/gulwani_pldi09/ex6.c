824eb18885cb9dfa894e41ad01a0ad65fe60d41d
timosantonopoulos <devnull@localhost>
2017-02-21 14:49:25 -0500

    adding fig4_5 example

--- programs/complexity/gulwani_pldi09/ex6.c
+++ programs/complexity/gulwani_pldi09/ex6.c
@@ -9,7 +9,7 @@ int mainQ(int n, int m, int isOne){
      assert (0 <= m);
      assert (0 <= isOne);
      assert (isOne <= 1);
-     
+
      int i = 0;
      int j = 0;
      int k = 0;
@@ -23,6 +23,7 @@ int mainQ(int n, int m, int isOne){
      //%%%traces: int n, int m, int i, int t
      //dig2: i - n - 1 == 0, -m - t <= 0, 2*m*n - n*t == 0, 2*m*t - (t*t) == 0, -i <= -1
      //NOTE: *** these equalities don't seem to capture the correct bound n+2m ? ***
+     //Timos: This is because we haven't added a t++ for the outer loop. I suspect once we do that we will get what we want.
      return 0;
 }
 
--- programs/complexity/gulwani_pldi09/fig4_5.c
+++ programs/complexity/gulwani_pldi09/fig4_5.c
@@ -1,1 +1,30 @@
 /* *** I don't really know what this dir = fwd means so cannot try this example*** */
+
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+int mainQ(int n, int m, int j){
+     assert (m > 0);
+     assert (n > m);
+     int i = m;
+     int t = 0;
+     while(i>0 && i < n){
+	  if (j > 0) {
+	       i++;
+	  }else{
+	       i--;
+	  }
+	  t++;
+     }
+
+     //NOTE: With DIG1 I got m^2*t - m*n*t + n*t^2 - t^3 == 0, whose solutions are t = n-m, t = m, t =0. They seem correct.
+     //j plays the role of a fixed truth value representing dir=fwd
+     return 0;
+}
+
+
+int main(int argc, char **argv){
+     mainQ(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+     return 0;
+}
